f91bd2aad82838749293f5ed843952ac2861c349
Сергей <fredrsf@fredrsf.(none)>
2012-05-18 11:14:42 +0400

    new

--- checkingLikes.php
+++ checkingLikes.php
@@ -12,12 +12,13 @@ $active_menu = 0;
 $globalTemplateParam->set('active_menu',$active_menu);
 /*-------активность главного меню--------*/
 
-$id = $user->id;
+//$id = $user->id;
+$id = 5;
 $api_id = '2629628';
 
 $user_object = new fmakeSiteUser();
 $object_likes_from_db = new promoLike_like();
-printAr($object_likes_from_db);
+
 $array_likes = $object_likes_from_db->getAllNewLikes($id);
 
 printAr($array_likes);
@@ -25,9 +26,13 @@ printAr($array_likes);
 $user_params = $user_object->getUserSocialParam($id, 2);
 $user_token = $user_params['tocken'];
 
+require './modules/APIvk/vkapi.php';
 $api_object = new fmakeVkapi($api_id, $user_token);
 
-printAr($user_token);
+$k = $api_object->desktop_api('wall.get ', array('count' => 3, 'filter' => 'owner'));
+//$k = $api_object->desktop_api('wall.get ');
+
+printAr($k);
 
 /*
 $globalTemplateParam->set('payments',$payments);
--- modules/promoLike/checkLikes.php
+++ modules/promoLike/checkLikes.php
@@ -6,11 +6,18 @@
  */
 class promoLike_checkLikes extends fmakeCore {
     public function getArrayLikes($json){
-        return $json;
+        $likes_array = json_decode($json, true);
+        return $likes_array;
     }
     
     public function searchLike($array_likes, $array_likes_from_db){
-        return false;
+        foreach ($array_likes as $key => $value) {
+            foreach ($array_likes_from_db as $like) {
+                if($value == $like){
+                    
+                }
+            }
+        }
     }
 }
 ?>
