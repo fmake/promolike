c0cec7a8e7b330bd17fcc2ff640c6226de1f1aba
Mamay <Mamay@192.168.1.3>
2012-06-05 01:17:27 +0400

    скрипт проверки на активность токена

---
+++ check_users_place.php
@@ -0,0 +1,40 @@
+<?
+require ('./libs/FController.php');
+require './modules/APIvk/vkapi.php';
+
+$promoLikePlace = new promoLike_socialset();
+$promoLikeUsers = new fmakeSiteUser();
+$fmakeSiteUsers = new fmakeSiteUser();
+$promoLikeUsers->idField = "id";
+$promoLikeUsers->table = $promoLikeUsers->table_social;
+
+
+$all_place_users = $promoLikeUsers->getAll();
+
+if($all_place_users)foreach ($all_place_users as $key=>$item){
+	switch ($item[$promoLikePlace->idField]){
+		case '1':
+			break;
+		case '2':			
+			$vk = new fmakeVkapi();
+			$api_id = '2629628';
+			$result = $vk->isUserTokenVK($api_id,$item[$fmakeSiteUsers->idField],$item[$promoLikePlace->idField]);
+			printAr($result);
+			if($result->response){
+				echo('qq<br/>');
+				$promoLikeUsers->setId($item['id']);
+				$promoLikeUsers->addParam('active',1);
+				$promoLikeUsers->update();
+			}
+			else{
+				$promoLikeUsers->setId($item['id']);
+				$promoLikeUsers->addParam('active',0);
+				$promoLikeUsers->update();
+			}
+			break;
+		case '3':
+			break;
+	}
+}
+
+?>
